1e63a9d299a86e1dd603ed09ba5cdf285712d337
Ronak Shah <ronakshahultimate@gmail.com>
2020-08-25 15:20:12 -0700

    moved setup to dockerfile (#2)
    * moved setup to dockerfile
    
    * added readme

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,16 @@
 FROM node:latest
 
-WORKDIR /tmp
-COPY entrypoint.sh /tmp/entrypoint.sh
-ENTRYPOINT ["/tmp/entrypoint.sh"]
+RUN apt update
+RUN apt install apt-transport-https curl git sudo -y
+RUN echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+RUN curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+RUN apt update
+RUN sudo apt-get install sbt -y
+RUN git clone https://github.com/opticdev/optic /home/node/optic
+WORKDIR /home/node/optic
+RUN git checkout feature/spec-publisher
+RUN /bin/bash -c "source ./sourceme.sh && optic_build"
 
+WORKDIR /home/node
+COPY entrypoint.sh /home/node/entrypoint.sh
+ENTRYPOINT ["/home/node/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,9 @@
 #!/bin/bash -l
 
-echo https://github.com/$1
-apt update
-apt install sudo -y
-sudo apt-get update
-sudo apt-get install apt-transport-https curl git -y
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
-sudo apt-get update
-sudo apt-get install sbt -y
-
-cd /tmp
-git clone https://github.com/opticdev/optic optic
-cd optic
-git checkout feature/spec-publisher
-
-. ./sourceme.sh && optic_build
-cd /tmp
-
 git clone https://github.com/$1 repoName
 cd repoName
 git checkout $2
 export OPTIC_LOCAL_CLI__API_GATEWAY=https://staging.infra.opticdev.com
 export OPTIC_LOCAL_SPEC_VIEWER_URL=https://apidocs.o3c.info
-/tmp/optic/workspaces/ci-cli/bin/run publish  > output
-cat output
-export MESSAGE=$(cat output)
-echo $MESSAGE
+export MESSAGE=$(/home/node/optic/workspaces/ci-cli/bin/run publish)
 echo "::set-output name=message::$MESSAGE"

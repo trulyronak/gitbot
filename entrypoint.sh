7b57a442ec58c19edf68a7f865d863f360f846a0
Ronak Shah <ronakshahultimate@gmail.com>
2020-08-26 15:24:25 -0700

    made change to the api

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -l
 
 git clone https://github.com/$1 repoName
+# we currently have base
+cp .optic/api/specification.json /tmp/base.json
 cd repoName
 git checkout $2
+cp .optic/api/specification.json /tmp/head.json
 export OPTIC_LOCAL_CLI__API_GATEWAY=https://staging.infra.opticdev.com
 export OPTIC_LOCAL_SPEC_VIEWER_URL=https://apidocs.o3c.info
-export MESSAGE=$(/home/node/optic/workspaces/ci-cli/bin/run publish)
+export URL=$(/home/node/optic/workspaces/ci-cli/bin/run publish --url)
+export MESSAGE=$(/home/node/optic/workspaces/ci-cli/bin/run compare /tmp/base.json /tmp/head.json --specUrl $URL)
 echo "::set-output name=message::$MESSAGE"
